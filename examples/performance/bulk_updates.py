bff8aea60c8a9638da7ebed2907c24cbb9338be2
Mykola <pogoreliy_na@groupbwt.com>
2019-12-24 17:50:09 +0200

    Add new expr'

--- examples/performance/bulk_updates.py
+++ examples/performance/bulk_updates.py
@@ -7,10 +7,11 @@ from sqlalchemy import Column, text
 from sqlalchemy import create_engine
 from sqlalchemy import Integer
 from sqlalchemy import String
-from sqlalchemy.dialects.mysql import insert
+
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import Session
 from performance import Profiler
+
 
 
 Base = declarative_base()
@@ -24,7 +25,7 @@ class Customer(Base):
     description = Column(String(255))
 
 
-Profiler.init("bulk_updates", num=100000)
+Profiler.init("bulk_updates", num=1000000)
 
 
 @Profiler.setup
@@ -54,10 +55,10 @@ def setup_database(dburl, echo, num):
 # def test_orm_flush(n):
 #     """UPDATE statements via the ORM flush process."""
 #     session = Session(bind=engine)
-#     for chunk in range(0, n, 1000):
+#     for chunk in range(0, n, 10000):
 #         customers = (
 #             session.query(Customer)
-#             .filter(Customer.id.between(chunk, chunk + 1000))
+#             .filter(Customer.id.between(chunk, chunk + 10000))
 #             .all()
 #         )
 #         for customer in customers:
@@ -106,35 +107,61 @@ def setup_database(dburl, echo, num):
 #         session.flush()
 #     session.commit()
 
-@Profiler.profile
-def test_orm_update(n):
-    '''Update statements via orm update() method'''
+# @Profiler.profile
+# def test_orm_update(n):
+#     '''Update statements via orm update() method'''
+#
+#     session = Session(bind=engine)
+#     # print(len(session.query(Customer).all()))
+#     # print(len(session.query(Customer).filter(Customer.description == "updated").all()))
+#     for chunk in range(0, n, 1000):
+#         customers = (
+#             session.query(Customer)
+#             .filter(Customer.id.between(chunk, chunk + 1000))
+#             .update(
+#                 {Customer.description: 'updated'},
+#                 synchronize_session=False
+#             )
+#         )
+#         session.flush()
+#     session.commit()
+#     # print(len(session.query(Customer).all()))
+#     # print(len(session.query(Customer).filter(Customer.description == "updated").all()))
+#
+# @Profiler.profile
+# def test_core_update(n):
+#     """A single Core UPDATE construct inserting mappings in bulk."""
+#     conn = Session(bind=engine)#engine.connect()
+#     s = Customer.__table__.update()
+#     for chunk in range(0, n, 1000):
+#         conn.execute(
+#             s,
+#             [
+#                 dict(
+#                     name="customer name %d" % i,
+#                     description="updated",
+#                 )
+#                 for i in range(chunk, chunk + 1000)
+#             ],
+#         )
+#         conn.flush()
+#     conn.commit()
 
-    session = Session(bind=engine)
-    # print(len(session.query(Customer).all()))
-    # print(len(session.query(Customer).filter(Customer.description == "updated").all()))
-    for chunk in range(0, n, 1000):
-        customers = (
-            session.query(Customer)
-            .filter(Customer.id.between(chunk, chunk + 1000))
-            .update(
-                {Customer.description: 'updated'},
-                synchronize_session=False
-            )
-        )
-        session.flush()
-    session.commit()
-    # print(len(session.query(Customer).all()))
-    # print(len(session.query(Customer).filter(Customer.description == "updated").all()))
+
+
+from sqlalchemy.dialects.mysql import insert as minsert
 
 @Profiler.profile
-def test_core_update(n):
-    """A single Core UPDATE construct inserting mappings in bulk."""
-    conn = Session(bind=engine)#engine.connect()
-    s = Customer.__table__.update()
+def test_mysql_update_or_create(n):
+    '''Update statements via MySQL insert() method'''
+
+    insert_stmt = minsert(Customer)
+
+    odk = insert_stmt.on_duplicate_key_update(**insert_stmt.inserted)
+    session = engine.connect()
     for chunk in range(0, n, 1000):
-        conn.execute(
-            s,
+        session.execute(
+            odk,
             [
                 dict(
                     name="customer name %d" % i,
@@ -143,28 +170,30 @@ def test_core_update(n):
                 for i in range(chunk, chunk + 1000)
             ],
         )
-        conn.flush()
-    conn.commit()
+
+
+from sqlalchemy.ext.compiler import compiles
+from sqlalchemy.sql.expression import Insert as Sinsert
+
+@compiles(Sinsert)
+def append_string(insert, compiler, **kw):
+    s = compiler.visit_insert(insert, **kw)
+    if insert.kwargs.get('mysql_append_string'):
+        fields = s[s.find("(") + 1:s.find(")")].replace(" ", "").split(",")
+        generated_directive = ["{0}=VALUES({0})".format(field) for field in fields]
+        return s + " ON DUPLICATE KEY UPDATE " + ",".join(generated_directive)
+    return s
+
+Sinsert.argument_for("mysql", "append_string", None)
 
 @Profiler.profile
 def test_core_update_or_create(n):
     '''Update statements via core execute() method'''
-    from sqlalchemy.ext.compiler import compiles
-    from sqlalchemy.sql.expression import Insert
 
-    @compiles(Insert)
-    def append_string(insert, compiler, **kw):
-        s = compiler.visit_insert(insert, **kw)
-        if insert.kwargs.get('mysql_append_string'):
-            fields = s[s.find("(") + 1:s.find(")")].replace(" ", "").split(",")
-            generated_directive = ["{0}=VALUES({0})".format(field) for field in fields]
-            return s + " ON DUPLICATE KEY UPDATE " + ",".join(generated_directive)
-        return s
-
-    Insert.argument_for("mysql", "append_string", None)
     session = engine.connect()#Session(bind=engine)
+    s = Customer.__table__.insert(mysql_append_string=True)
+    # print(str(s))
     for chunk in range(0, n, 1000):
-        s = Customer.__table__.insert(mysql_append_string=True)
         session.execute(
             s,
             [
@@ -176,7 +205,6 @@ def test_core_update_or_create(n):
             ],
         )
     # session.commit()
-
 
 if __name__ == "__main__":
     Profiler.main()
